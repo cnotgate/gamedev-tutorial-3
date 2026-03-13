423265943d0825c2e7829121407a88feaa7f653e
ahmad.fatih22 <ahmad.fatih22@ui.ac.id>
2026-03-13 21:10:27 +0700

    tutorial 5 done

--- Player.gd
+++ Player.gd
@@ -1,27 +1,35 @@
 extends CharacterBody2D
 
-@export var gravity = 200.0
-@export var walk_speed = 200
-@export var jump_speed = -300
-var double_jump_capability = true
+@export var SPEED := 200
+@export var JUMP_SPEED := -400
+@export var GRAVITY := 1200
+@onready var animplayer = $AnimatedSprite2D
 
-func _physics_process(delta):
-	velocity.y += delta * gravity
-	
-	if is_on_floor():
-		double_jump_capability = true
-	
-	if double_jump_capability and Input.is_action_just_pressed('ui_up'):
-		velocity.y = jump_speed
-		if not is_on_floor():
-			double_jump_capability = false
+const UP = Vector2(0,-1)
 
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -walk_speed
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  walk_speed
+func _get_input():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	var animation = "idle"
+	if direction:
+		animation = "walk right"
+		velocity.x = direction * SPEED
+		if direction>0:
+			animplayer.flip_h = false
+		else:
+			animplayer.flip_h = true
 	else:
-		velocity.x = 0
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	animplayer.play(animation)
 
-	# "move_and_slide" already takes delta time into account.
+	move_and_slide()
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += delta*GRAVITY
+	_get_input()
 	move_and_slide()
